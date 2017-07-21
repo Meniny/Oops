4290faffb003ba4193ecdd9ac7befea51ebe83e6
Github <meniny@qq.com>
2017-07-21 23:59:26 +0800

    typealias

---
+++ Oops/Typealias.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+public typealias SCLAlertView = Oops
+public typealias OopsAlertView = Oops
+public typealias OopsView = Oops
+
+public typealias SCLButton = Oops.Button
+public typealias OopsButton = Oops.Button
